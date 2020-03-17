cf5653cee5544b61ac548466802c86e6f3695b44
wangfan <wangfan@ejiayou.com>
2020-03-17 09:49:28 +0800

    1

--- src/main/java/com/wfan/server/EchoServerHandler.java
+++ src/main/java/com/wfan/server/EchoServerHandler.java
@@ -20,16 +20,8 @@ public class EchoServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf buf = (ByteBuf) msg;
-        String str;
-        if (buf.hasArray()) { // 处理堆缓冲区
-            str = new String(buf.array(), buf.arrayOffset() + buf.readerIndex(), buf.readableBytes());
-        } else { // 处理直接缓冲区以及复合缓冲区
-            byte[] bytes = new byte[buf.readableBytes()];
-            buf.getBytes(buf.readerIndex(), bytes);
-            str = new String(bytes, 0, buf.readableBytes());
-        }
-        System.out.println("Server received: " + str);
+        System.out.println("Server received: " + ByteBufUtil
+            .hexDump((ByteBuf) msg));
         ctx.write(msg);
     }
 
